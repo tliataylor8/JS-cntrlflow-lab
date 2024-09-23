c9e3fc2e39201bed5bf991022d9f8f16579808a5
tliataylor8 <140763571+tliataylor8@users.noreply.github.com>
2024-09-23 12:06:52 -0400

    part 1 of lab needs revision

---
+++ main.js
@@ -0,0 +1,44 @@
+
+// const PI = 3.1415;
+// const radius = 5;
+// const gardenCap = PI * radius * radius;
+
+// let eachPlant = 0.8;
+
+// let w1Plants= 20; 
+// let w2Plants= w1Plants * 2;
+// let w3Plants= w2Plants * 2;
+// let w4Plants= w3Plants * 2;
+
+// // #0.5
+// let maxPlants = gardenCap / eachPlant;
+// console.log(`Max plants allowed in garden area: ${maxPlants}`)
+
+// // Implement flow control
+// // **check your functions for the ifelse for each week
+// // w1
+// if (w1Plants>(0.8*maxPlants)){
+//     console.log(`Prune`)  
+// } else if (0.8>w1Plants>0.5){
+//     console.log(`Monitor`)
+// } else if (w1Plants < 0.5) {
+//     console.log(`Plant`)
+// }
+// // w2
+// if (w2Plants>(0.8*maxPlants)){
+//     console.log(`Prune`)  
+// } else if (0.8>w2Plants>0.5){
+//     console.log(`Monitor`)
+// } else if (w2Plants < 0.5) {
+//     console.log(`Plant`)
+// }
+// // w3
+// if (w3Plants>(0.8*maxPlants)){
+//     console.log(`Prune`)  
+// } else if (0.8>w3Plants>0.5){
+//     console.log(`Monitor`)
+// } else if (w3Plants < 0.5) {
+//     console.log(`Plant`)
+// }
+
+
